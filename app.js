e384721afe7751523def1e6ed69c8a3d0e42decf
AppHero2 <bearkingbrother@gmail.com>
2018-06-12 15:36:28 +0800

    sms

--- app.js
+++ app.js
@@ -34,13 +34,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  // res.header("Access-Control-Allow-Credentials", true);
-  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  // res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", '*');
+//   // res.header("Access-Control-Allow-Credentials", true);
+//   // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   // res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//   next();
+// });
 
 app.use('/', index);
 app.use('/users', users);
